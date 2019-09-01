c9c1a19699c406e6a65148904c7f07132206bd27
bekazandukeli <bekazandukeli@gmail.com>
2019-09-01 15:26:24 +0400

    i finally overcame lit exercise

--- 04_3_a_list.js
+++ 04_3_a_list.js
@@ -1,14 +1,43 @@
 function arrayToList(array) {
-    let reversed = array.reverse(); 
-    let list = {};
-    for (let element of reversed) {
-        list.value = element;
-        list.rest = list;
+	let list = null;
+  	for (i = array.length - 1; i >= 0; i--) {
+    	list = {value: array[i], rest: list};
     }
-    return list;
+	return list;
 }
 
+function listToArray(list) {
+	let array = [];
+  	while (true) {
+  		array.push(list.value);
+  		if (list.rest == null) break;
+      	list = list.rest;
+    }
+	return array;
+}
 
+function prepend(a, list) {
+	list = {value: a, rest: list};
+  	return list;
+}
 
-
-console.log(1,2,3);
+function nth(list, num) {
+  	let value;
+	for (let i = 0; i <= num; i++) {
+      if (list == null) {
+        return undefined; 
+        break;
+      }
+      value = list.value;
+      list = list.rest;
+    }
+  	return value;
+}   
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 2));
+// → 20
