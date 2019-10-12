d932aa53e7f39866a9feaa86cd287e7626e5fbbb
bekazandukeli <bekazandukeli@gmail.com>
2019-10-12 23:38:36 +0400

    completed fixed bugs, works well

--- 04_4_deep_comparison.js
+++ 04_4_deep_comparison.js
@@ -16,41 +16,49 @@
 
 function deepEqual(x, y) {
     
-	if (typeof x == "object" && typeof y == "object") {
-    
-        let counter = 0;
-
-    	for (let propertyX of Object.keys(x)) {
-
-        	for (let propertyY of Object.keys(y)) {
-
-                if ((propertyX == propertyY) && (typeof x[propertyX] !== "object" && typeof y[propertyY] !== "object")) {
-                   
-                    if (x[propertyX] == y[propertyY]) {
-                       
-                        counter++;
-                   
+	if (typeof x == "object" && typeof y == "object") {  // თუ ობიექტებია ვაგრძელებთ, თუ არა, წავედით 40-ე ხაზზე.
+        if (Object.keys(x).length !== Object.keys(y).length) return false; // ამით შესაძლოა რესურსი დაიზოგოს.
+        else { 
+                let counter = 0;
+                // ყოველ x-ის property name-ს ვადარებ ყველა y-ისას.
+                for (let propertyX of Object.keys(x)) {
+                    for (let propertyY of Object.keys(y)) {
+                        // თუ რომელიმე ორი property name ერთმანეთს დაემთხვა და მათგან არც ერთი არაა 
+                        // ობიექტი მაშინ ვადარებთ მათ value-ებს და ტოლობის შემთხვევაში counter++.
+                        // სხვა შემთხვევაში გადავდივართ 32 ხაზზე და ვლოცულობთ, რომ იმუშაოს >_<
+                        if (propertyX == propertyY) {
+                            if (typeof x[propertyX] !== "object" && typeof y[propertyY] !== "object") {
+                                if (x[propertyX] == y[propertyY]) counter++;
+                                debugger;
+                            } else if (deepEqual(x[propertyX], y[propertyY])) counter++; /* <------ დიიახ! დიიახ! დიიაააახ!!! ეს რეკურსიააა! ^_^  */  
+                        } 
                     }
-               
-                } 
-                
-                else return deepEqual(x[propertyX], y[propertyY]);   /* <------ დიიახ! დიიახ! დიიაააახ!!! ეს რეკურსიააა :****  */   
-                       	
-            }
-
-        }
-
+                }
+            // და აი დადგა გადამწყვეტი მომენტი...    
+            if (counter == Object.keys(x).length) return true;
+            else return false;
+        }    
     }
-
     return x == y; 
-
 }
 
 let obj = {here: {is: "an"}, object: 2};
 
+
+
+
 console.log(deepEqual(obj, obj));
 // → true
+console.log(deepEqual(1, 2));
+// → false
+console.log(deepEqual("რამე", "რამე"));
+// → true
+console.log(deepEqual("რამე", "რუმე"));
+// → false
 console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual(obj, {object: 2, here: {is: "an"}}));
+// → true
+
+console.log(deepEqual({object: 2, sharzeVar: {here: {is: "an"}}}, {sharzeVar: {here: {is: "an"}}, object: 2}));
 // → true
