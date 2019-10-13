cfd14f91cce2d78b8dc7a940db5da7f07cca1566
bekazandukeli <bekazandukeli@gmail.com>
2019-10-13 17:00:58 +0400

    removed comments

--- 04_4_deep_comparison.js
+++ 04_4_deep_comparison.js
@@ -1,21 +1,14 @@
 function deepEqual(x, y) {
-    
-	if (typeof x == "object" && typeof y == "object") {  // თუ ობიექტებია ვაგრძელებთ, თუ არა, წავედით 26-ე ხაზზე.
+    if (typeof x == "object" && typeof y == "object") {
         if (Object.keys(x).length !== Object.keys(y).length) return false; 
-        // ზემოთ სიგრძეების ტოლობა მნიშვნელოვანია, რადგან თუ ერთ-ერთი ობიექტის ელემენტები მეორე ობიექტის 
-        // ელემენტების ქვესიმრავლე იქნება და counter-ს 22-ე ხაზზე ამ პატარა ობიექტს შევადარებთ შეცდომით true-ს დაგვიბრუნდება. 
         else { 
                 let counter = 0;
-                // ქვემოთ ყოველ x-ის property name-ს ვადარებ ყველა y-ისას.
                 for (let propertyX of Object.keys(x)) {
                     for (let propertyY of Object.keys(y)) {
-                        // თუ რომელიმე ორი property name ერთმანეთს დაემთხვა და მათგან არც ერთი არაა 
-                        // ობიექტი მაშინ ვადარებთ მათ value-ებს და ტოლობის შემთხვევაში counter++.
-                        // სხვა შემთხვევაში გადავდივართ მე-18 ხაზზე და ვლოცულობთ, რომ იმუშაოს >_<
                         if (propertyX == propertyY) {
                             if (typeof x[propertyX] !== "object" && typeof y[propertyY] !== "object") {
                                 if (x[propertyX] == y[propertyY]) counter++;
-                            } else if (deepEqual(x[propertyX], y[propertyY])) counter++; /* <------ დიიახ! დიიახ! დიიაააახ!!! ეს რეკურსიააა! ^_^  */  
+                            } else if (deepEqual(x[propertyX], y[propertyY])) counter++;  
                         } 
                     }
                 }    
@@ -25,23 +18,3 @@ function deepEqual(x, y) {
     }
     return x == y; 
 }
-
-let obj = {here: {is: "an"}, object: 2};
-
-
-
-
-console.log(deepEqual(obj, obj));
-// → true
-console.log(deepEqual(1, 2));
-// → false
-console.log(deepEqual(obj, {here: 1, object: 2}));
-// → false
-console.log(deepEqual(obj, {object: 2, here: {is: "an"}}));
-// → true
-console.log(deepEqual({object: 2, "შარზევარ": {here: {is: "an"}}}, {"შარზევარ": {here: {is: "an"}}, object: 2}));
-// → true
-console.log(deepEqual(["ტორტი", 15, 27], ["ტორტი", 15, 27]));
-// → true
-console.log(deepEqual(["ბადრიჯანი", 15, 27], ["ტორტი", 15, 27]));
-// → false
